27a65b6ce96615e8ea2f2397513b8ef020ea7693
leandroblanca <ezepiolablank@gmail.com>
2026-03-07 12:21:02 -0300

    feature:Primer commit, creacion de proyecto

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Route, Routes } from 'react-router'
+import './App.css'
+import "./index.css";
+import { Router } from 'react-router'
+
+function App() {
+  
+
+  return (
+    <>
+    <BrowserRouter>
+    <Routes>
+      <Route>
+        
+      </Route>
+    </Routes>
+    </BrowserRouter>
+    </>
+  )
+}
+
+export default App
