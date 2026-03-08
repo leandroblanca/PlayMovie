e31bd3ebf13f1ce396c19772b34b58f72a614f34
Marisol Lamas <marilamas02@gmail.com>
2026-03-07 21:38:01 -0300

    agrego estructura visual de admin

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import "./index.css";
 import AdminProtegida from "./pages/Admin/AdminProtegida";
-import Login from './pages/login';
 
 function App() {
   
@@ -12,7 +11,6 @@ function App() {
     <BrowserRouter>
     <Routes>
        <Route path="/admin" element={<AdminProtegida />} />
-       <Route path="/login" element={<Login />} />
     </Routes>
     </BrowserRouter>
     </>
--- src/pages/Admin/Admin.jsx
+++ src/pages/Admin/Admin.jsx
@@ -1,12 +1,67 @@
-import React from 'react'
+import { Button, Modal, Table, Form } from "react-bootstrap";
+import { useState } from "react";
+import "./Admin.css";
 
-const Admin = () => {
+
+function Admin() {
+   const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
-    <div>
-        <h1>AdminPage</h1>
-      
-    </div>
-  )
+    <>
+      <Button variant="danger" onClick={handleShow}>
+        Agregar pelicula
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Agregar Pelicula</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body >
+          <Form>
+            <Form.Control className="mb-3"
+              type="text"
+              placeholder="Buscar película..."
+            />
+
+            <Form.Control className="mb-3"
+              type="text"
+              placeholder="Título"
+            />
+
+            <Form.Control className="mb-3"
+              type="text"
+              placeholder="Año"
+            />
+
+            <Form.Control className="mb-3"
+              type="text"
+              placeholder="URL del poster"
+            />
+
+          </Form>
+            <Button variant="danger"type="submit">Agregar</Button>
+        </Modal.Body>
+
+     
+      </Modal>
+
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Título</th>
+            <th>Año</th>
+            <th>Poster</th>
+            <th>Acciones</th>
+          </tr>
+        </thead>
+        <tbody></tbody>
+      </Table>
+    </>
+  );
 }
 
-export default Admin
+export default Admin;
