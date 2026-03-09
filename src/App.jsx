64fbf8af44fd7b48a032f1c614aec474bf0c61f1
Marisol Lamas <marilamas02@gmail.com>
2026-03-09 13:00:32 -0300

    agrego estructura de login con logica

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Nav from "./componentes/Nav";
 import Footer from "./componentes/Footer";
 import AdminProtegida from './pages/Admin/Admin';
 import Contacto from './pages/Contacto/Contacto';
+import Login from './pages/Login/Login';
 
 
 
@@ -19,7 +20,7 @@ function App() {
       <Routes>
         <Route path="/admin" element={<AdminProtegida/>} />
         <Route path="/contacto" element={<Contacto/>} />
-        
+        <Route path="/login" element={<Login/>} />
     </Routes>
     <Footer />
     </BrowserRouter>
