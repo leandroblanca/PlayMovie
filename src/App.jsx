ba0cdcb56c0c5bd2d255e2017490fb4e7136f1cd
Sebas2411 <mastropiero2411@gmail.com>
2026-03-09 17:35:56 -0300

    Feature AboutUs: se inicia construcción de pagina aboutus y se modifica navbar con link a aboutus y tambien se modifica app.jsx con react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import { BrowserRouter, Route, Routes } from 'react-router'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css'
 import "./index.css";
-import { Router } from 'react-router'
 import Nav from "./componentes/Nav";
 import Footer from "./componentes/Footer";
+import AboutUs from "./pages/AboutUs"
 
 function App() {
   
@@ -16,6 +16,8 @@ function App() {
         <Route path="/inicio" element={<h1>Inicio</h1>} />
         <Route path="/peliculas" element={<h1>Películas</h1>} />
         <Route path="/series" element={<h1>Series</h1>} />
+        <Route path="/aboutus" element={<AboutUs />} />
+
     </Routes>
     <Footer />
     </BrowserRouter>
