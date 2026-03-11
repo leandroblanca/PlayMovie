d8e2e05f27c4efec7b4057f6035fc1f6df0cc2ad
Marisol Lamas <marilamas02@gmail.com>
2026-03-11 13:03:49 -0300

    feat: Agrega usuarios por defecto a la carpeta data

---
+++ src/data/users.js
@@ -0,0 +1,22 @@
+export const usuarios = [
+  {
+    id: 1,
+    nombre: "Alex Johnson",
+    estado: "ACTIVO",
+    ultimoAcceso: "hace 2h"
+  },
+  {
+    id: 2,
+    nombre: "Sarah Miller",
+    estado: "ACTIVO",
+    ultimoAcceso: "hace 5h"
+  },
+  {
+    id: 3,
+    nombre: "Michael Brown",
+    estado: "INACTIVO",
+    ultimoAcceso: "hace 1 día"
+  }
+];
+
+export default usuarios;
