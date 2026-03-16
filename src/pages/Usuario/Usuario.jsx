248f5ab477c266b441841860402ee5f2b1833a25
Abel <abel.ruiz.852@gmail.com>
2026-03-16 13:54:37 -0300

    Creo constantes de nombre, email, clave y genero

--- src/pages/Usuario/Usuario.jsx
+++ src/pages/Usuario/Usuario.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 import "./Usuario.css"
 
 const Usuario = () => {
+  const [nombre, setNombre] = useState("Alex Rivers")
+  const [email, setEmail] = useState("alex.rivers@gmail.com")
+  const [clave, setClave] = useState("")
+  const [genero, setGenero] = useState("hombre")
   return (
     <>
       <div className='Perfil-Info'>
-        <img src="" 
-        alt="Foto de Usuario" />
+        <img src=""
+          alt="Foto de Usuario" />
         <div className='User'>
           <h1></h1>
           <p className='Email'></p>
