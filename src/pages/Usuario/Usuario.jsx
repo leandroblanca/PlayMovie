158307b63ca08411171adbfddbdaf60b6812831f
Abel <abel.ruiz.852@gmail.com>
2026-03-16 03:02:08 -0300

    Agrego 2 imagenes de avatares de perfil

---
+++ src/pages/Usuario/Usuario.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Usuario = () => {
+  return (
+    <>
+      <div className='Perfil-Info'>
+        <img src="" 
+        alt="Foto de Usuario" />
+      </div>
+    </>
+  )
+}
+
+export default Usuario
