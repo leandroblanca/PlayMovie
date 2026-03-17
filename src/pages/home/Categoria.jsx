23e2653db565cda09c8365096e6de410d7f9e41e
leandroblanca <ezepiolablank@gmail.com>
2026-03-17 20:51:06 -0300

    Agregue el useParams,tambien agregue useNavigate

--- src/pages/home/Categoria.jsx
+++ src/pages/home/Categoria.jsx
@@ -4,3 +4,8 @@ import { Button, Card, Container, Row, Col } from "react-bootstrap";
 import "./categoria.css";
 import { useState } from "react";
 import ModalPelicula from "./ModalHome";
+
+function CategoriaPage() {
+  const { gender } = useParams();
+  const navigate = useNavigate();
+  const generoDecodificado = decodeURIComponent(gender);
