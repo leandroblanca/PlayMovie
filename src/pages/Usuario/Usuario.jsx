a22a3a8badfa68ebe631abc4a925155e4f3535bf
Abel <abel.ruiz.852@gmail.com>
2026-03-17 02:28:38 -0300

    Creo funcion para guardar cambios

--- src/pages/Usuario/Usuario.jsx
+++ src/pages/Usuario/Usuario.jsx
@@ -23,77 +23,82 @@ const Usuario = () => {
     setGenero(usuario.genero);
     setAbrirModal(false);
   };
-  
-return (
-  <div className='Perfil'>
-    <div className='Perfil-Info'>
-      <img className='Avatar' src={genero === "hombre" ? Hombre : Mujer}
-        alt="Foto de Usuario" />
-      <div className='User'>
-        <h1>{nombre}</h1>
-        <p className='Email'>{email}</p>
-        <span><strong className='Premium'>MIEMBRO PREMIUM</strong></span>
-      </div>
-      <div className='Botones'>
-        <button className='Editar'>Editar Perfil</button>
-        <button className='Cerrar-Sesion'>Cerrar Sesión</button>
-      </div>
-    </div>
-    <div className='Estadisticas'>
-      <div className='stat'>
-        <h2>124</h2>
-        <h5>Peliculas</h5>
-        <p className='Letras'>VISTOS</p>
-      </div>
-      <div className='stat'>
-        <h2>45</h2>
-        <h5>Series</h5>
-        <p className='Letras'>VISTOS</p>
-      </div>
-      <div className='stat'>
-        <h2>12</h2>
-        <h5>Documentales</h5>
-        <p className='Letras'>VISTOS</p>
-      </div>
-    </div>
-    <div className='tabs'>
-      <button className='Item'>Ajustes de cuenta</button>
-      <button className='Item'>Subscripción</button>
-      <button className='Extra'>Mis favoritos</button>
-    </div>
-    <div className='Contenido'>
-      <div className='Panel-Izquierdo'>
-        <h4>Plan de Subscripción</h4>
-        <div className='Plan'>
-          <p className='Hd'><strong className='Hd'>ULTRA HD 4K</strong></p>
-          <span className='Mes'><strong className='Precio'>15.99</strong>/Mes</span>
+  const guardarCambios = (e) => {
+    e.preventDefault();
+    setAbrirModal(false)
+    if (clave !== "") { }
+  };
+
+  return (
+    <div className='Perfil'>
+      <div className='Perfil-Info'>
+        <img className='Avatar' src={genero === "hombre" ? Hombre : Mujer}
+          alt="Foto de Usuario" />
+        <div className='User'>
+          <h1>{nombre}</h1>
+          <p className='Email'>{email}</p>
+          <span><strong className='Premium'>MIEMBRO PREMIUM</strong></span>
         </div>
-        <li>
-          <FaCheckCircle className='Icono' /> 4 Pantallas a la vez
-        </li>
-        <li>
-          <FaCheckCircle className='Icono' /> Ultra HD disponible
-        </li>
-        <li>
-          <FaCheckCircle className='Icono' /> Descargas ilimitadas
-        </li>
-      </div>
-      <div className='Panel-Derecho'>
-        <h3 className='Reciente'>Agregados Recientemente a Favoritos</h3>
-        <div className='Fav'>
-          <h3 className='Favoritos'>Mis Favoritos</h3>
-          <button className='VerTodo'>Ver Todo</button>
-        </div>
-        <div className='Peliculas'>
-          <div className='Pelis'></div>
-          <div className='Pelis'></div>
-          <div className='Pelis'></div>
-          <div className='Pelis'></div>
+        <div className='Botones'>
+          <button className='Editar'>Editar Perfil</button>
+          <button className='Cerrar-Sesion'>Cerrar Sesión</button>
         </div>
       </div>
+      <div className='Estadisticas'>
+        <div className='stat'>
+          <h2>124</h2>
+          <h5>Peliculas</h5>
+          <p className='Letras'>VISTOS</p>
+        </div>
+        <div className='stat'>
+          <h2>45</h2>
+          <h5>Series</h5>
+          <p className='Letras'>VISTOS</p>
+        </div>
+        <div className='stat'>
+          <h2>12</h2>
+          <h5>Documentales</h5>
+          <p className='Letras'>VISTOS</p>
+        </div>
+      </div>
+      <div className='tabs'>
+        <button className='Item'>Ajustes de cuenta</button>
+        <button className='Item'>Subscripción</button>
+        <button className='Extra'>Mis favoritos</button>
+      </div>
+      <div className='Contenido'>
+        <div className='Panel-Izquierdo'>
+          <h4>Plan de Subscripción</h4>
+          <div className='Plan'>
+            <p className='Hd'><strong className='Hd'>ULTRA HD 4K</strong></p>
+            <span className='Mes'><strong className='Precio'>15.99</strong>/Mes</span>
+          </div>
+          <li>
+            <FaCheckCircle className='Icono' /> 4 Pantallas a la vez
+          </li>
+          <li>
+            <FaCheckCircle className='Icono' /> Ultra HD disponible
+          </li>
+          <li>
+            <FaCheckCircle className='Icono' /> Descargas ilimitadas
+          </li>
+        </div>
+        <div className='Panel-Derecho'>
+          <h3 className='Reciente'>Agregados Recientemente a Favoritos</h3>
+          <div className='Fav'>
+            <h3 className='Favoritos'>Mis Favoritos</h3>
+            <button className='VerTodo'>Ver Todo</button>
+          </div>
+          <div className='Peliculas'>
+            <div className='Pelis'></div>
+            <div className='Pelis'></div>
+            <div className='Pelis'></div>
+            <div className='Pelis'></div>
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
-)
+  )
 }
 
 export default Usuario
