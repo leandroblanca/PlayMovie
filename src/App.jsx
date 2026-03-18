f5a0a4ebc9e9c5f956861614c93a9f144c448e76
leandroblanca <ezepiolablank@gmail.com>
2026-03-18 10:23:45 -0300

    Hice los ultimos cambios del home

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./componentes/NavBar/NavBar";
-
-
-// Páginas de ejemplo (creá tus componentes reales en /pages)
 import Home from "./pages/home/Home";
 import Contacto from "./pages/Contacto/Contacto";
 import Error404 from "./pages/error404/Error404";
 import AboutUs from "./pages/AboutUs/AboutUs";
 import Login from "./pages/Login/Login";
 import Registro from "./pages/Registro/Registro";
-import Contacto from "./pages/Contacto/Contacto";
 import { useEffect } from 'react';
 import { usuariosIniciales } from './helpers/users';
 import peliculasIniciales from './data/movies';
+import CategoriaPage from "./pages/home/Categoria";
+import Footer from './componentes/footer/Footer'
 
 function App() {
   useEffect(() => {
@@ -33,15 +31,17 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
         <Route path="/contacto" element={<Contacto />} />
         <Route path="/error404" element={<Error404 />} />
         <Route path="/nosotros" element={<AboutUs />} />
         <Route path="/login" element={<Login />} />
         <Route path="/registro" element={<Registro />} />
         <Route path="*" element={<Error404 />} />
+        <Route path="/" element={<Home peliculas={peliculasIniciales} />} />
+        <Route path="/categoria/:gender" element={<CategoriaPage />} />
       </Routes>
       <Footer/>
     </Router>
   );
 }
+export default App;
