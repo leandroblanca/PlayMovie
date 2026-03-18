4c0bba329b34df719ee02d233012700af9db7a59
leandroblanca <ezepiolablank@gmail.com>
2026-03-17 21:17:36 -0300

    importe las rutas

---
+++ src/pages/home/ModalHome.jsx
@@ -0,0 +1,6 @@
+import React, { useState } from "react"
+import { Container, Row, Col, Card, Button, Carousel} from "react-bootstrap";
+import categorias from "../../data/categories";
+import { useNavigate } from "react-router";
+import './home.css';
+import ModalPelicula from "./ModalHome"
