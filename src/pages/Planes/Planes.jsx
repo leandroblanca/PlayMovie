42cff011a19af27ea3d09b24aa86c985dddee28b
Abel <abel.ruiz.852@gmail.com>
2026-03-19 03:23:27 -0300

    Importo iconos de bootstrap y termino el body de planes

--- src/pages/Planes/Planes.jsx
+++ src/pages/Planes/Planes.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import "./Planes.css"
+import { FaCheckCircle, FaTimesCircle } from "react-icons/fa";
 
 const Planes = () => {
     return (
@@ -7,7 +8,7 @@ const Planes = () => {
             <section className='Title'>
                 <h1 className='Titulo'><strong>Todo el cine,</strong>
                     <strong className='Titulo-Rojo'> a tu alcance.</strong></h1>
-                <p className='Parrafo'>Elige el plan que mejor se adapte a ti. 
+                <p className='Parrafo'>Elige el plan que mejor se adapte a ti.
                     Cancela cuando quieras, sin <br /> compromisos.</p>
             </section>
             <div className='Contenedor'>
@@ -17,6 +18,20 @@ const Planes = () => {
                         <h3>$7.99<span className='Mes'>/mes</span></h3>
                         <p className='Letras'>Ideal para smartphones y tables.</p>
                     </div><hr />
+                    <div className='Card-Body'>
+                        <li>
+                            <FaCheckCircle className='Icono' /> <span className='Text'>Calidad SD (480p)</span> 
+                        </li>
+                        <li>
+                            <FaCheckCircle className='Icono' /> <span className='Text'>1 dispositivo a la vez</span> 
+                        </li>
+                        <li>
+                            <FaCheckCircle className='Icono' /> <span className='Text'>Catálogo completo</span> 
+                        </li>
+                        <li>
+                            <FaTimesCircle className='Icono-Tachado' /> <span className='Text-Tachado'>Descargas offline</span> 
+                        </li>
+                    </div><hr />
                 </div>
                 <div className='Card-Rojo'>
                     <div className='Header'>
@@ -24,12 +39,40 @@ const Planes = () => {
                         <h3>$12.99<span className='Mes'>/mes</span></h3>
                         <p className='Letras'>Para compartir en alta definición.</p>
                     </div><hr />
+                    <div className='Card-Body'>
+                        <li>
+                            <FaCheckCircle className='Icono' /> <span className='Text'>Calidad full HD (1080p)</span> 
+                        </li>
+                        <li>
+                            <FaCheckCircle className='Icono' /> <span className='Text'>2 dispositivo a la vez</span> 
+                        </li>
+                        <li>
+                            <FaCheckCircle className='Icono' /> <span className='Text'>Descargas disponibles</span>
+                        </li>
+                        <li>
+                            <FaCheckCircle className='Icono' /> <span className='Text'>Sin anuncios</span> 
+                        </li>
+                    </div><hr />
                 </div>
                 <div className='Card'>
                     <div className='Header'>
                         <h2>Premium</h2>
                         <h3>$17.99<span className='Mes'>/mes</span></h3>
                         <p className='Letras'>La experiencia definitiva de cine.</p>
+                    </div><hr />
+                    <div className='Card-Body'>
+                        <li>
+                            <FaCheckCircle className='Icono' /> <span className='Text'>Calidad Ultra HD (4K) + HDR</span>
+                        </li>
+                        <li>
+                            <FaCheckCircle className='Icono' /> <span className='Text'>4 dispositivo a la vez</span> 
+                        </li>
+                        <li>
+                            <FaCheckCircle className='Icono' /> <span className='Text'>Sonido Dolby Atmos</span>
+                        </li>
+                        <li>
+                            <FaCheckCircle className='Icono' /> <span className='Text'>Descargas en 10 dispositivos</span>
+                        </li>
                     </div><hr />
                 </div>
             </div>
