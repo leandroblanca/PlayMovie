ae0ea078957225dd7627647f91ada68fafadeb11
Abel <abel.ruiz.852@gmail.com>
2026-03-18 23:10:43 -0300

    Agrego titulo principial y le pongo diseño

--- src/pages/Planes/Planes.jsx
+++ src/pages/Planes/Planes.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import "./Planes.css"
 
 const Planes = () => {
-  return (
-    <div>
-        
-    </div>
-  )
+    return (
+        <div>
+            <section className='Title'>
+                <h1 className='Titulo'><strong>Todo el cine,</strong>
+                    <strong className='Titulo-Rojo'> a tu alcance.</strong></h1>
+            </section>
+        </div>
+    )
 }
 
 export default Planes
