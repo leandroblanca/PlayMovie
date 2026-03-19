88e09052454b76bf3af76bef17291df79a6831c0
Abel <abel.ruiz.852@gmail.com>
2026-03-18 23:35:34 -0300

    Agrego parrafo centrado

--- src/pages/Planes/Planes.jsx
+++ src/pages/Planes/Planes.jsx
@@ -7,6 +7,8 @@ const Planes = () => {
             <section className='Title'>
                 <h1 className='Titulo'><strong>Todo el cine,</strong>
                     <strong className='Titulo-Rojo'> a tu alcance.</strong></h1>
+                <p className='Parrafo'>Elige el plan que mejor se adapte a ti. 
+                    Cancela cuando quieras, sin <br /> compromisos.</p>
             </section>
         </div>
     )
