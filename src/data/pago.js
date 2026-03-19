b23359c970883a1858441de6676971f4a3d394a0
Marisol Lamas <marilamas02@gmail.com>
2026-03-19 17:35:29 -0300

    feat: Agrega impotacion de imagen de pago en pago.js

--- src/data/pago.js
+++ src/data/pago.js
@@ -1,10 +1,12 @@
+import Pago from "../../public/assets/pago.png"
+
 const suscripciones = [
   {
     id: 1,
     plan: "Plan Premium 4K + HDR",
     precio: "14,99 € / mes",
     totalHoy: "14,99 €",
-    img: "/pago.png",
+    img: Pago,
   },
 ];
 
