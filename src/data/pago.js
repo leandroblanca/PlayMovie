0e829a670c2bde47bd5e1dadf0dc247bdf84489f
Marisol Lamas <marilamas02@gmail.com>
2026-03-18 21:24:39 -0300

    feat: fix: Agrega ruta de imagen a pago.js

--- src/data/pago.js
+++ src/data/pago.js
@@ -1,9 +1,11 @@
- const pago = [
-    {
-      id: 1,
-      plan: "Plan Premium 4K + HDR",
-      precio: "14,99 € / mes",
-      totalHoy: "14,99 €",
-      img: "https://via.placeholder.com/80"
-    }
-  ];
+const suscripciones = [
+  {
+    id: 1,
+    plan: "Plan Premium 4K + HDR",
+    precio: "14,99 € / mes",
+    totalHoy: "14,99 €",
+    img: "/pago.png",
+  },
+];
+
+export default suscripciones;
