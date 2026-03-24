933bd94b0339f47a6a0e380ada89d05c04119bff
Marisol Lamas <marilamas02@gmail.com>
2026-03-24 17:28:13 -0300

    fix: Importa rutas al navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,91 +1,68 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from "react-router-dom";
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./componentes/NavBar/NavBar";
 import Home from "./pages/home/Home";
 import Error404 from "./pages/error404/Error404";
 import AboutUs from "./pages/AboutUs/AboutUs";
 import Login from "./pages/Login/Login";
 import Registro from "./pages/Registro/Registro";
-import { useEffect } from 'react';
 import { usuariosIniciales } from './helpers/users';
 import peliculasIniciales from './data/movies';
 import "@fontsource/poppins";
 import CategoriaPage from "./pages/home/Categoria";
-import Footer from './componentes/footer/Footer'
-import Contacto from './pages/Contacto/Contacto'
+import Footer from './componentes/footer/Footer';
+import Contacto from './pages/Contacto/Contacto';
 import PasarelaDePago from "./pages/PasarelaDePago/Pasarela";
 import Admin from "./pages/Admin/Admin";
 import RutaProtegida from "./componentes/RutaProtegida";
 import Usuario from "./pages/Usuario/Usuario";
-<<<<<<< HEAD
 import Planes from "./pages/Planes/Planes";
-import CentroAyuda from "./pages/CentroAyuda/CentroAyuda";
+import Ayuda from "./pages/CentroAyuda/CentroAyuda";
 import DetallePelicula2 from "./pages/DetallePelicula2/DetallePelicula2";
 import Favoritos from "./pages/Favoritos/Favoritos";
 
-
-
-=======
-import Ayuda from "./pages/CentroAyuda/CentroAyuda"
-import DetallePelicula2 from "./pages/DetallePelicula2/DetallePelicula2";
-import Favoritos from "./pages/favoritos/Favoritos"
-
-import Planes from "./pages/Planes/Planes"
-import Favoritos from "./pages/favoritos/Favoritos"
-import DetallePelicula2 from "./pages/DetallePelicula2/DetallePelicula2"
-import CentroAyuda from "./pages/CentroAyuda/CentroAyuda"
->>>>>>> d4ef2d8e5652f47875b1e02660bb06013615adcc
-
 function App() {
   useEffect(() => {
-    function primeraCarga () {
-      const peliculas = JSON.parse(localStorage.getItem("peliculas"));
-      const usuarios = JSON.parse(localStorage.getItem("usuarios"));
-      if (!peliculas || peliculas.length === 0) {
-        localStorage.setItem("peliculas", JSON.stringify(peliculasIniciales));
-      }
-      if (!usuarios || usuarios.length === 0) {
-        const usuariosLimpios = usuariosIniciales.map(u => {
-          const { password, ...resto } = u;
-          return resto;
-        });
-        localStorage.setItem("usuarios", JSON.stringify(usuariosLimpios));
-      }
-  
-      else {
-        const usuariosSanitizados = usuarios.map(u => {
-          const { password, ...resto } = u;
-          return resto;
-        });
-        localStorage.setItem("usuarios", JSON.stringify(usuariosSanitizados));
-      }
+    const VERSION = "v2";
+    const peliculas = JSON.parse(localStorage.getItem("peliculas"));
+    if (!peliculas || peliculas.length === 0 || localStorage.getItem("peliculas_version") !== VERSION) {
+      localStorage.setItem("peliculas", JSON.stringify(peliculasIniciales));
+      localStorage.setItem("peliculas_version", VERSION);
     }
-    primeraCarga();
-  }, [])
+
+    const usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
+    const sanitizados = usuarios.length
+      ? usuarios.map(({ password, ...u }) => u)
+      : usuariosIniciales.map(({ password, ...u }) => u);
+    localStorage.setItem("usuarios", JSON.stringify(sanitizados));
+  }, []);
+
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/contacto" element={<Contacto />} />
-        <Route path="/usuario" element={<Usuario />} />
-        <Route path="/error404" element={<Error404 />} />
-        <Route path="/pago" element={<PasarelaDePago />} />
-        <Route path="/nosotros" element={<AboutUs />} />
-        <Route path="/planes" element={<Planes />} />
+        <Route path="/" element={<RutaProtegida><Home /></RutaProtegida>} />
         <Route path="/login" element={<Login />} />
         <Route path="/registro" element={<Registro />} />
+        <Route path="/contacto" element={<Contacto />} />
+        <Route path="/usuario" element={<Usuario />} />
+        <Route path="/nosotros" element={<AboutUs />} />
+        <Route path="/planes" element={<Planes />} />
         <Route path="/ayuda" element={<Ayuda />} />
+        <Route path="/pago" element={<PasarelaDePago />} />
+        <Route path="/favoritos" element={<Favoritos />} />
         <Route path="/detallepelicula/:id" element={<DetallePelicula2 />} />
-        <Route path="*" element={<Error404 />} />
+        <Route path="/categoria/:gender" element={<CategoriaPage />} />
+        <Route path="/admin" element={<Admin />} />
         <Route path="/usuarios" element={<Admin />} />
         <Route path="/ingresos" element={<Admin />} />
         <Route path="/peliculas" element={<Admin />} />
-        <Route path="/" element={<RutaProtegida><Home peliculas={peliculasIniciales} /></RutaProtegida>} />
-        <Route path="/categoria/:gender" element={<CategoriaPage />} />
-        <Route path="/favoritos" element={<Favoritos />} />
+        <Route path="/error404" element={<Error404 />} />
+        <Route path="*" element={<Error404 />} />
       </Routes>
-      <Footer/>
-      </Router>
+      <Footer />
+    </Router>
   );
 }
+
 export default App;
